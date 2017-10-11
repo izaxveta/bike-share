07c4572887ef92d910f1e501fd97f4fd8e369a64
BLaurenB <blbillington1@gmail.com>
2017-10-11 09:31:01 -0600

    did lots of circling without any real progress.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -13,4 +13,13 @@ class Condition < ActiveRecord::Base
   )
 
 
+
+
 end
+
+=begin
+* Breakout of average number of rides, highest number of rides, and lowest number of rides on days with a high temperature in 10 degree chunks (e.g. average number of rides on days with high temps between fifty and sixty degrees)
+* Breakout of average number of rides, highest number of rides, and lowest number of rides on days with precipitation in half-inch increments.
+* Breakout of average number of rides, highest number of rides, and lowest number of rides on days with mean wind speeds in four mile increments.
+* Breakout of average number of rides, highest number of rides, and lowest number of rides on days with mean visi
+=end
