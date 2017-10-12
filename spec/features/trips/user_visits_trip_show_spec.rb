5c61a8a390584bc29281bd7f0b929f6cdf0d3f4c
Josi <junipertmcclellan@gmail.com>
2017-10-12 00:08:17 -0600

    delete accidental merge text

--- spec/features/trips/user_visits_trip_show_spec.rb
+++ spec/features/trips/user_visits_trip_show_spec.rb
@@ -1,6 +1,7 @@
 require './spec/spec_helper'
 
 feature 'when a user visits trip show page' do
+
   context('for a trip that doesn\'t exist') do
     background { visit 'trips/0' }
 
@@ -17,16 +18,12 @@ feature 'when a user visits trip show page' do
     end
 
     it 'they see the end station' do
-<<<<<<< HEAD
       expect(page).to have_content('Harry Bridges Plaza (Ferry Building)')
-=======
-      expect(page).to have_content(/end station/i)
       expect(page).to have_content('7')
     end
 
     it 'they see the start date' do
       expect(page).to have_content('2013-08-31')
->>>>>>> master
     end
 
     it 'they see the date' do
@@ -34,40 +31,28 @@ feature 'when a user visits trip show page' do
     end
 
     it 'they see the duration' do
-<<<<<<< HEAD
       expect(page).to have_content(/duration/i)
       expect(page).to have_content('556 minutes')
-=======
-      expect(page).to have_content('3743')
->>>>>>> master
     end
 
     it 'they see the bike id' do
       expect(page).to have_content(/bike id/i)
-<<<<<<< HEAD
       expect(page).to have_content('342')
     end
 
     it 'they see the subscription type' do
       expect(page).to have_content(/subscription/i)
       expect(page).to have_content('Subscriber')
-=======
-      expect(page).to have_content('100')
     end
 
     it 'they see the subscription type' do
       expect(page).to have_content(/subscription type/i)
       expect(page).to have_content('Customer')
->>>>>>> master
     end
 
     it 'they see the zipcode' do
       expect(page).to have_content(/zip code/i)
-<<<<<<< HEAD
       expect(page).to have_content('94123')
-=======
-      expect(page).to have_content('19803')
->>>>>>> master
     end
 
     it 'they see a delete button' do
@@ -81,13 +66,17 @@ feature 'when a user visits trip show page' do
     context 'when user clicks on delete button' do
       background { click_button 'delete' }
 
-      it 'then user is redirected to trip index page' do
+      it 'they are redirected to trip index page' do
         has_current_path?('/trips', only_path: true)
-        expect(page).to have_content(/delete successful/i)
       end
 
-      it 'the user sees the deleted trip is not listed' do
+      it 'they see the deleted trip is not listed' do
+        expect(page).to_not have_content('#6342')
+      end
+
+      it 'they see a success message' do
         skip
+        expect(page).to have_content(/delete successful/i)
       end
 
     end
