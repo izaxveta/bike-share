0116ef7bc0151e0d193c44566b6660f939d779bd
seoulstice <jungyoungjoon@gmail.com>
2017-10-11 22:30:02 -0600

    Station_dashboard_spec fixed

--- spec/features/stations/user_visits_station_dashboard_spec.rb
+++ spec/features/stations/user_visits_station_dashboard_spec.rb
@@ -4,28 +4,28 @@ feature 'When a user visits the Station Dashboard' do
 
   background { visit '/station-dashboard' }
 
-  it 'they see the total docks' do
-    expect(page).to have_content(/total docks/i)
+  it 'they see the total stations' do
+    expect(page).to have_content('Total docks')
   end
 
   it 'they see the average dock count' do
-    expect(page).to have_content(/average bikes/i)
+    expect(page).to have_content('Average bikes available per station')
   end
 
   it 'they see the stations with fewest docks' do
-    expect(page).to have_content(/fewest bikes/i)
+    expect(page).to have_content('Stations with least bikes')
   end
 
   it 'they see the stations with most docks' do
-    expect(page).to have_content(/most bikes/i)
+    expect(page).to have_content('Stations with most bikes')
   end
 
   it 'they see the oldest station(s)' do
-    expect(page).to have_content(/oldest/i)
+    expect(page).to have_content('Oldest Stations')
   end
 
   it 'they see the newest station(s)' do
-    expect(page).to have_content(/newest/i)
+    expect(page).to have_content('Newest stations')
   end
 
 end
